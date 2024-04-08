bc97b69ed0113c660ad7bbcd4459635d2458b192
kharrisonJO <kristine@juicyorange.com>
2024-04-08 16:27:58 -0400

    Add hero 3

---
+++ hero3/script.js
@@ -0,0 +1,77 @@
+/* https://github.com/fuzzywalrus/Bootstrap---JQuery-Cycle--Responsive-Slide-Show-.git */
+
+
+$(document).ready(function() {
+  //JQuery Cycle code
+      $('#slideshow').after('<ul id="pager">').cycle({ 
+      fx:     'fade',
+      //prev: '#prev',
+      //next: '#next',
+      //pager:  '#pager',
+      containerResize: 0,
+      width: '100%',
+      fit: 1,
+        pagerAnchorBuilder: function(idx, el) {
+        return '<li><a href="cpage_7#"></a></li>';
+        }
+    });
+  
+  //This resizes the blank image in the resize example
+    //initglitch fix 
+    var initfix = 18;
+  
+    ///on resize, set blank image height
+    function imageresize() {
+      $(".slidecontainer").each(function () {
+          var imgHeight = $(this).find(".slide1").height();
+          $(this).find(".blankimg").css("height", imgHeight+initfix);
+        });
+    };
+    $(window).bind("resize", function(){
+      //clears the initfix
+      initfix = 0;
+      imageresize();
+    });  
+    imageresize();
+  });
+  
+  //Additional Inits for demos of slide show
+  
+  $(document).ready(function() {
+      $('#slideshow2').after('<ul id="pager2">').cycle({ 
+      fx:     'fade',
+      //prev: '#prev',
+      //next: '#next',
+      //pager:  '#pager',
+      containerResize: 0,
+      width: '100%',
+      fit: 1,
+      pagerAnchorBuilder: function(idx, el) {
+      return '<li><a href="cpage_7#"></a></li>';
+      }
+    });  
+    $('#slideshow3').after('<ul id="pager3">').cycle({ 
+      fx:     'fade',
+      //prev: '#prev',
+      //next: '#next',
+      //pager:  '#pager',
+      containerResize: 0,
+      width: '100%',
+      fit: 1,
+      pagerAnchorBuilder: function(idx, el) {
+      return '<li><a href="cpage_7#"></a></li>';
+      }
+    }); 
+    $('#slideshow4').after('<ul id="pager3">').cycle({ 
+      fx:     'fade',
+      //prev: '#prev',
+      //next: '#next',
+      //pager:  '#pager',
+      containerResize: 0,
+      width: '100%',
+      fit: 1,
+      pagerAnchorBuilder: function(idx, el) {
+      return '<li><a href="cpage_7#"></a></li>';
+      }
+    });  
+  });
